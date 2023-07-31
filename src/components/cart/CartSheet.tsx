279084f31934fe3fb751695b822ce018e0cde078
SAFWAT <safwathkhan2@gmail.com>
2023-07-31 22:21:11 +0530

    added cart to nav

--- src/components/cart/CartSheet.tsx
+++ src/components/cart/CartSheet.tsx
@@ -1,26 +1,39 @@
-import { Sheet, SheetTrigger } from "@/components/ui/sheet"
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet"
 import { Button } from "@/components/ui/button"
 import { Icons } from "@/components/icons"
 import { Badge } from "@/components/ui/badge";
 
 const CartSheet = async () => {
 
-    const cartLineItems = []
+    const cartLineItems = [1]
 
     const itemsCount = cartLineItems.length;
 
   return (
     <Sheet>
         <SheetTrigger>
-            <Button>
+            <Button
+                aria-label="Open cart"
+                variant="outline"
+                size="icon"
+                className="relative"
+            >
                 {itemsCount > 0 && (
-                    <Badge >
+                    <Badge 
+                        variant="secondary"
+                        className="absolute -top-2 -right-2 w-6 h-5 rounded-full p-2"
+                    >
                         {itemsCount}
                     </Badge>
                 )}
-                <Icons.cart className="w-6 h-6" aria-hidden="true"/>
+                <Icons.cart className="w-4 h-4" aria-hidden="true"/>
             </Button>
         </SheetTrigger>
+        <SheetContent>
+            <SheetHeader>
+                <SheetTitle>Cart{itemsCount > 0 && `(${itemsCount})`}</SheetTitle>
+            </SheetHeader>
+        </SheetContent>
     </Sheet>
   )
 }
